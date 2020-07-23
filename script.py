c88aaa920a603830282f8935caf1702744346a21
Ravinder Deol <ravinder.r.deol@gmail.com>
2020-07-23 16:20:44 +0300

    It's now dynamic. User inputs are possible.

--- script.py
+++ script.py
@@ -1,18 +1,28 @@
-player_1 = "Sam"
-player_2 = "Alex"
-current_round = 1
+player_1 = input("What Is The Name Of Player 1? ")
+
+player_2 = input("What Is The Name Of Player 2? ")
+
+current_round = int(input("What Is The Current Round? "))
 
 print("Game On!")
+
 print(f"Player 1: {player_1}")
+
 print(f"Player 2: {player_2}")
+
 print("--------------")
 
 print(f"Round {current_round}!")
-player_1_score = 10
-player_2_score = 13
-print(f"{player_2} wins with {player_2_score} to {player_1_score}")
-print("-----------------------")
 
-print(f"Round {current_round + 1}!")
-player_1_score = 20
-player_2_score = 5
+player_1_score = int(input(f"What Is {player_1} Score? "))
+
+player_2_score = int(input(f"What Is {player_2} Score? "))
+
+if player_1_score > player_2_score:
+    print(f"{player_1} Wins!")
+elif player_1_score < player_2_score:
+    print(f"{player_2} Wins!")
+else:
+    print("It's A Draw!")
+
+print("-----------------------")
